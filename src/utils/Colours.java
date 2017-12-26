a760cb159c98013c041e66152680bd8bf1e9d4da
Cacior Mihai <mihaicacior@yahoo.com>
2017-12-26 17:13:36 +0200

    Commit

--- src/utils/Colours.java
+++ src/utils/Colours.java
@@ -8,7 +8,7 @@ public class Colours {
     public static final int BLACK = -16777216;
     public static final int Dark_Blue = -16777165;
     public static final int Light_Blue = -16758921;
-    public static final int Magenta = -7336897;
+    public static final int Magenta = -2527442 ;//-7336897;
     public static final int Brown = -11721216;
     public static final int Light_Brown = -6724045;
     public static final int Green = -16738048;
